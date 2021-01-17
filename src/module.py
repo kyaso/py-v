dcd5d39f257faf8262a240af85795a267509d76a
Kirthihan Yasotharan <kitti2295+git@gmail.com>
2021-01-17 17:46:13 +0100

    Start implementing pipeline stages

---
+++ src/module.py
@@ -0,0 +1,4 @@
+# TODO: Maybe make this abstract
+class Module:
+    def process(self):
+        raise Exception('Please implement process() for this module')
---
+++ src/stages.py
@@ -0,0 +1,68 @@
+from module import *
+from port import *
+from reg import *
+
+# class Stage:
+#     def __init__(self):
+#         pass
+
+#     A_i = Port()
+#     B_i = Port()
+#     C_o = Port()
+
+#     def process(self):
+#         self.C_o.val = self.A_i.val + self.B_i.val
+    
+class FetchStage(Module):
+    def __init__(self):
+        self.npc_i = Port()
+        self.inst_i = Port()
+        self.inst_o = Port()
+        self.pc_o = Port()
+
+        self.PC = 0
+        self.IR = 0
+    
+    def process(self):
+        # Read inputs
+        self.IR = self.inst_i.val # TODO: Read instruction mem
+        self.PC = self.npc_i.val
+
+        # Outputs
+        self.inst_o.val = self.IR
+        self.pc_o.val = self.PC
+
+class DecodeStage(Module):
+    def __init__(self, regf):
+        self.regfile = regf
+
+        self.inst_i = Port()
+        self.pc_i = Port()
+        
+        self.rs1_o = Port()
+        self.rs2_o = Port()
+        self.imm_o = Port()
+        self.pc_o = Port()
+        self.rd_o = Port()
+        self.we_o = Port(False)
+        
+    def process(self):
+        # Determine register indeces
+        rs1_idx = 0 # TODO
+        rs2_idx = 0
+
+        # Read regfile
+        rs1 = self.regfile.read(rs1_idx)
+        rs2 = self.regfile.read(rs2_idx)
+
+        # Decode immediate
+        imm = 0 # TODO
+
+        # Outputs
+        self.rs1_o.val = rs1
+        self.rs2_o.val = rs2
+        self.imm_o.val = imm
+        self.pc_o.val = self.pc_i.val
+        self.rd_o.val = 0 # TODO
+        self.we_o.val = 0 # TODO
+
