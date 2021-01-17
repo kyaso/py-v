69daafb35c23c871303098638b85608a011b5719
Kirthihan Yasotharan <kitti2295+git@gmail.com>
2021-01-17 22:15:35 +0100

    Add opcode definitions

---
+++ src/isa.py
@@ -0,0 +1,54 @@
+OPCODES = {
+    "LOAD"      : 0x00,
+    "OP-IMM"    : 0x04,
+    "AUIPC"     : 0x05,
+    "STORE"     : 0x08,
+    "OP"        : 0x0C,
+    "LUI"       : 0x0D,
+    "BRANCH"    : 0x18,
+    "JALR"      : 0x19,
+    "JAL"       : 0x1B
+}
+
+# --------------------------------
+# Instruction formats
+# --------------------------------
+
+INST_R = [
+    OPCODES["OP"]
+]
+
+INST_I = [
+    OPCODES["LOAD"],
+    OPCODES["OP-IMM"],
+    OPCODES["JALR"]
+]
+
+INST_S = [
+    OPCODES["STORE"]
+]
+
+INST_B = [
+    OPCODES["BRANCH"]
+]
+
+INST_U = [
+    OPCODES["AUIPC"],
+    OPCODES["LUI"]
+]
+
+INST_J = [
+    OPCODES["JAL"]
+]
+
+# Instructions that write back into the register file
+# REG_OPS = [
+#     OPCODES["LOAD"],
+#     OPCODES["OP-IMM"],
+#     OPCODES["AUIPC"],
+#     OPCODES["OP"],
+#     OPCODES["LUI"],
+#     OPCODES["JALR"],
+#     OPCODES["JAL"]
+# ]
+REG_OPS = INST_R + INST_I + INST_U + INST_J
