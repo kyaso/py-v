4e35c0654291a7f6b7bd86c3a0d6d4e03cb58ff3
Kirthihan Yasotharan <kitti2295+git@gmail.com>
2021-01-23 17:25:05 +0100

    Add multivalue ports

--- src/port.py
+++ src/port.py
@@ -7,3 +7,13 @@ class Port:
     
     def write(self, val):
         self.val = val
+    
+class PortX(Port):
+    def read(self, port_key=None):
+        if port_key is None:
+            return self.val
+        else:
+            return self.val[port_key]
+    
+    def write(self, val):
+        self.val.update(val)
--- test/test_port.py
+++ test/test_port.py
@@ -8,3 +8,27 @@ def test_port():
     B = A
     A.write(0x42)
     assert B.read() == 0x42
+
+def test_portx():
+    init = {'one':1, 'two':2, 'three':3}
+
+    # Test init
+    A = PortX(init)
+    assert A.val == init
+
+    # Test reading all subports
+    ret = A.read()
+    assert ret == init
+
+    # Test reading one subport
+    ret = A.read('one')
+    assert ret == 1
+
+    # Test reading another subport
+    ret = A.read('three')
+    assert ret == 3
+
+    # Test write
+    A.write({'one':42, 'two':45})
+    assert A.val == {'one':42, 'two':45, 'three':3}
+
