0e99e0c7f08e68c38a917817264a346128942b73
Kirthihan Yasotharan <kirthihan.y@gmail.com>
2023-12-20 15:43:51 +0100

    revert tmps

--- test/test_util.py
+++ test/test_util.py
@@ -13,7 +13,7 @@ def test_getBits():
     assert getBits(3, 1, 1) == 1
     assert getBits(3, 0, 0) == 1
     assert getBits(15, 3, 2) == 3
-    assert getBits(0xdeadbeef, 31, 1) == 0x6F56DF78
+    assert getBits(0xdeadbeef, 31, 1) == 0x6F56DF77
 
 
 def test_getBitVector():
