f8d745f5d08bd876915c47d27c68c8f060cd10d8
Kirthihan Yasotharan <kirthihan.y@gmail.com>
2023-12-20 15:23:45 +0100

    style: pep8 test/test_util.py

--- test/test_util.py
+++ test/test_util.py
@@ -1,10 +1,12 @@
 import pytest
-from pyv.util import *
+from pyv.util import getBit, getBits, getBitVector
+
 
 def test_getBit():
     assert getBit(1, 0) == 1
     assert getBit(1, 1) == 0
     assert getBit(8, 3) == 1
+
 
 def test_getBits():
     assert getBits(3, 1, 0) == 3
@@ -13,9 +15,9 @@ def test_getBits():
     assert getBits(15, 3, 2) == 3
     assert getBits(0xdeadbeef, 31, 1) == 0x6F56DF77
 
-def test_getBitVector():
-    assert getBitVector(0xAA, 0) == [1,0,1,0,1,0,1,0]
-    assert getBitVector(0xAA, 11) == [0,0,0,1,0,1,0,1,0,1,0] 
-    with pytest.warns(UserWarning):
-        assert getBitVector(0x39, 4) == [1,0,0,1]
 
+def test_getBitVector():
+    assert getBitVector(0xAA, 0) == [1, 0, 1, 0, 1, 0, 1, 0]
+    assert getBitVector(0xAA, 11) == [0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0]
+    with pytest.warns(UserWarning):
+        assert getBitVector(0x39, 4) == [1, 0, 0, 1]
