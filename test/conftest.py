0f815dd59dd013e9ed0985d4146fbc3ac4801982
Kirthihan Yasotharan <kirthihan.y@gmail.com>
2023-12-20 14:04:29 +0100

    style: pep8 test/conftest.py

--- test/conftest.py
+++ test/conftest.py
@@ -1,11 +1,12 @@
 import pytest
-from pyv.clocked import Clock
 from pyv.simulator import Simulator
+
 
 @pytest.fixture(autouse=True)
 def clear_sim():
     yield
     Simulator.clear()
+
 
 @pytest.fixture
 def sim() -> Simulator:
