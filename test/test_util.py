633861abf7102a1c2701c7d7141602c6b39ddb67
Kirthihan Yasotharan <kirthihan.y@gmail.com>
2023-12-20 15:43:00 +0100

    tmp2

--- test/test_util.py
+++ test/test_util.py
@@ -13,7 +13,8 @@ def test_getBits():
     assert getBits(3, 1, 1) == 1
     assert getBits(3, 0, 0) == 1
     assert getBits(15, 3, 2) == 3
-    assert getBits(0xdeadbeef, 31, 1) == 0x6F56DF77
+    assert getBits(0xdeadbeef, 31, 1) == 0x6F56DF78
+
 
 def test_getBitVector():
     assert getBitVector(0xAA, 0) == [1, 0, 1, 0, 1, 0, 1, 0]
