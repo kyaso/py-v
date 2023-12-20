fb598831349c318c96bf27bba3044428ac912e81
Kirthihan Yasotharan <kirthihan.y@gmail.com>
2023-12-20 12:47:33 +0100

    style: pep8 log.py

--- pyv/log.py
+++ pyv/log.py
@@ -1,5 +1,6 @@
 import logging
 
+
 def getLogger():
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
@@ -10,11 +11,12 @@ def getLogger():
     stream_handler = logging.StreamHandler()
     file_handler = logging.FileHandler("run.log", 'w')
     stream_handler.setFormatter(formatter)
-    #file_handler.setFormatter(formatter)
+    # file_handler.setFormatter(formatter)
 
     # logger.addHandler(stream_handler)
     logger.addHandler(file_handler)
 
     return logger
+
 
 logger = getLogger()
