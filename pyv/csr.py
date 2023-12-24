4d67c77bdfbab8fef8cace0896dfb03058085053
Kirthihan Yasotharan <kirthihan.y@gmail.com>
2023-12-24 20:17:33 +0100

    wip

--- pyv/csr.py
+++ pyv/csr.py
@@ -3,40 +3,68 @@ from pyv.port import Input, Output
 from pyv.reg import Reg
 from pyv.util import Container
 
+class CSRBlock(Module):
+    def __init__(self):
+        super().__init__()
+        self._csr_reg = Reg(int)
+        self.we_i = Input(bool)
+        self.write_val_i = Input(int)
+        self.csr_val_o = Output(int)
+
+        self.csr_val_o << self._csr_reg.cur
+
+    def process(self):
+        val = self._csr_reg.cur.read()
+        if self.we_i.read():
+            val = self.write_val_i.read()
+        self._csr_reg.next.write(val)
+
 
 class CSRBank(Container):
     def __init__(self):
         super().__init__()
-        self.misa = Reg(int, 0x42)
 
+        self.misa = CSRBlock()
 
 class CSRUnit(Module):
     def __init__(self):
         super().__init__()
 
         self.csr_bank = CSRBank()
+
         # TODO: interesting for docs: we shouldn't nest declare PyObjs inside
         # non-PyObjs -> Use Container class
-        self._CSR_MAP = {
-            0x301: self.csr_bank.misa
-        }
-
         self.csr_num_i = Input(int)
-        self.write_val_i = Input(int, [self.write])
-        self.write_en_i = Input(bool, [self.write])
+        self.write_val_i = Input(int, [None])
+        self.write_en_i = Input(bool, [self.write, self.validate_read_en_write_en])
+        self.read_en_i = Input(bool, [self.read, self.validate_read_en_write_en])
         self.read_val_o = Output(int)
+
+        self.csr_bank.misa.write_val_i << self.write_val_i
 
     def process(self):
         self.read()
         self.write()
 
+    def validate_read_en_write_en(self):
+        if self.read_en_i.read() and self.write_en_i.read():
+            raise Exception("CSR: Cannot read and write at same time!")
+
     def read(self):
-        addr = self.csr_num_i.read()
-        csr_val = self._CSR_MAP[addr].cur.read()
-        self.read_val_o.write(csr_val)
+        read_val = 0
+        if self.read_en_i.read():
+            addr = self.csr_num_i.read()
+            if addr == 0x301:
+                read_val = self.csr_bank.misa.csr_val_o.read()
+
+        self.read_val_o.write(read_val)
+
+    def _disable_write(self):
+        self.csr_bank.misa.we_i.write(False)
 
     def write(self):
+        self._disable_write()
         if self.write_en_i.read():
             addr = self.csr_num_i.read()
-            write_val = self.write_val_i.read()
-            self._CSR_MAP[addr].next.write(write_val)
+            if addr == 0x301:
+                self.csr_bank.misa.we_i.write(True)
--- test/test_csr.py
+++ test/test_csr.py
@@ -1,6 +1,28 @@
 import pytest
-from pyv.csr import CSRUnit
+from pyv.csr import CSRBlock, CSRUnit
 from pyv.simulator import Simulator
+
+@pytest.fixture
+def csr_block() -> CSRBlock:
+    csr = CSRBlock()
+    csr._init()
+    return csr
+
+class TestCSRBlock:
+    def test_read(self, sim: Simulator, csr_block: CSRBlock):
+        csr_block._csr_reg.cur._val = 0x42
+        assert csr_block.csr_val_o.read() == 0x42
+
+    def test_write(self, sim: Simulator, csr_block: CSRBlock):
+        csr_block.we_i.write(True)
+        csr_block.write_val_i.write(0x123)
+        sim.step()
+        assert csr_block.csr_val_o.read() == 0x123
+
+        csr_block.we_i.write(False)
+        csr_block.write_val_i.write(0x3)
+        sim.step()
+        assert csr_block.csr_val_o.read() == 0x123
 
 
 @pytest.fixture
@@ -9,28 +31,48 @@ def csr_unit() -> CSRUnit:
     csr_unit._init()
     return csr_unit
 
+def csr_write(csr_unit: CSRUnit, csr_num: int, value: int, sim: Simulator):
+    csr_unit.csr_num_i.write(csr_num)
+    csr_unit.write_en_i.write(True)
+    csr_unit.write_val_i.write(value)
+    sim.step()
+
+def csr_read(csr_unit: CSRUnit, csr_num: int, sim: Simulator):
+    csr_unit.read_en_i.write(True)
+    csr_unit.csr_num_i.write(csr_num)
+    sim.step()
+    return csr_unit.read_val_o.read()
+
+def write_and_read(csr_unit: CSRUnit, csr_num: int, value: int, sim: Simulator):
+    csr_write(csr_unit, csr_num, value, sim)
+    return csr_read(csr_unit, csr_num, sim)
+
 
 class TestCSRUnit:
     def test_csr_read(self, sim: Simulator, csr_unit: CSRUnit):
         misa = 0x301
-        csr_unit.csr_bank.misa.cur._val = 0x2
-        csr_unit.csr_num_i.write(misa)
-        sim.step()
-        assert csr_unit.read_val_o.read() == 0x2
+        csr_unit.csr_bank.misa._csr_reg.cur._val = 0x456
+        assert csr_read(csr_unit, misa, sim) == 0x456
 
     def test_csr_write(self, sim: Simulator, csr_unit: CSRUnit):
         misa = 0x301
+        csr_write(csr_unit, misa, 0x1234, sim)
+        assert csr_unit.csr_bank.misa._csr_reg.cur._val == 0x1234
+
+    def test_csr_write_not_enable(self, sim: Simulator, csr_unit: CSRUnit):
+        misa = 0x301
+        csr_unit.csr_bank.misa._csr_reg.cur._val = 0x1234
         csr_unit.csr_num_i.write(misa)
-
-        csr_unit.write_en_i.write(True)
-        csr_unit.write_val_i.write(0x1234)
-        sim.step()
-        assert csr_unit.csr_bank.misa.cur._val == 0x1234
-
         csr_unit.write_en_i.write(False)
         csr_unit.write_val_i.write(0x43)
         sim.step()
-        assert csr_unit.csr_bank.misa.cur._val == 0x1234
+        assert csr_unit.csr_bank.misa._csr_reg.cur._val == 0x1234
+
+    def test_read_and_write_at_same_time(self, sim: Simulator, csr_unit: CSRUnit):
+        csr_unit.read_en_i.write(True)
+        csr_unit.write_en_i.write(True)
+        with pytest.raises(Exception):
+            sim.step()
 
     def test_non_existent_csr(self):
         assert False, "Test needs to be implemented"
@@ -42,3 +84,4 @@ class TestCSRUnit:
             to the latter set of registers
         """
         assert False, "Test needs to be implemented"
+
