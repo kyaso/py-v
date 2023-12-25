8cc2267146da9344e7737335daa52a2a7ddd282e
Kirthihan Yasotharan <kirthihan.y@gmail.com>
2023-12-25 01:09:44 +0100

    wip

--- pyv/csr.py
+++ pyv/csr.py
@@ -2,11 +2,13 @@ from pyv.module import Module
 from pyv.port import Input, Output
 from pyv.reg import Reg
 from pyv.util import Container
+from pyv.log import logger
 
 class CSRBlock(Module):
-    def __init__(self):
+    def __init__(self, reset_val, read_only = False):
         super().__init__()
-        self._csr_reg = Reg(int)
+        self.read_only = read_only
+        self._csr_reg = Reg(int, reset_val)
         self.we_i = Input(bool)
         self.write_val_i = Input(int)
         self.csr_val_o = Output(int)
@@ -15,7 +17,7 @@ class CSRBlock(Module):
 
     def process(self):
         val = self._csr_reg.cur.read()
-        if self.we_i.read():
+        if not self.read_only and self.we_i.read():
             val = self.write_val_i.read()
         self._csr_reg.next.write(val)
 
@@ -23,41 +25,52 @@ class CSRBlock(Module):
 class CSRBank(Container):
     def __init__(self):
         super().__init__()
+        self.misa = CSRBlock(0x4000_0100, read_only=False)
 
-        self.misa = CSRBlock()
+    def get_csr(self, addr) -> CSRBlock:
+        if addr == 0x301:
+            return self.misa
+        else:
+            logger.warn(f"CSR: Ignoring access to invalid/unimplemented CSR {addr}.")
+
+
+class CSROutMux(Module):
+    def __init__(self):
+        super().__init__()
+        # Inputs
+        self.select_i = Input(int)
+        self.misa_i = Input(int)
+
+        # Output
+        self.out_o = Output(int)
+
+    def process(self):
+        addr = self.select_i.read()
+        out_val = 0
+        if addr == 0x301:
+            out_val = self.misa_i.read()
+
+        self.out_o.write(out_val)
 
 class CSRUnit(Module):
     def __init__(self):
         super().__init__()
-
         self.csr_bank = CSRBank()
+        self.out_mux = CSROutMux()
 
         # TODO: interesting for docs: we shouldn't nest declare PyObjs inside
         # non-PyObjs -> Use Container class
-        self.csr_num_i = Input(int)
+        self.csr_num_i = Input(int, [self.write])
         self.write_val_i = Input(int, [None])
-        self.write_en_i = Input(bool, [self.write, self.validate_read_en_write_en])
-        self.read_en_i = Input(bool, [self.read, self.validate_read_en_write_en])
+        self.write_en_i = Input(bool, [self.write])
         self.read_val_o = Output(int)
 
         self.csr_bank.misa.write_val_i << self.write_val_i
 
-    def process(self):
-        self.read()
-        self.write()
-
-    def validate_read_en_write_en(self):
-        if self.read_en_i.read() and self.write_en_i.read():
-            raise Exception("CSR: Cannot read and write at same time!")
-
-    def read(self):
-        read_val = 0
-        if self.read_en_i.read():
-            addr = self.csr_num_i.read()
-            if addr == 0x301:
-                read_val = self.csr_bank.misa.csr_val_o.read()
-
-        self.read_val_o.write(read_val)
+        # Out mux
+        self.out_mux.select_i << self.csr_num_i
+        self.out_mux.misa_i << self.csr_bank.misa.csr_val_o
+        self.read_val_o << self.out_mux.out_o
 
     def _disable_write(self):
         self.csr_bank.misa.we_i.write(False)
@@ -66,5 +79,5 @@ class CSRUnit(Module):
         self._disable_write()
         if self.write_en_i.read():
             addr = self.csr_num_i.read()
-            if addr == 0x301:
-                self.csr_bank.misa.we_i.write(True)
+            csr = self.csr_bank.get_csr(addr)
+            csr.we_i.write(True)
--- test/test_csr.py
+++ test/test_csr.py
@@ -1,10 +1,11 @@
 import pytest
+from pyv.clocked import Clock
 from pyv.csr import CSRBlock, CSRUnit
 from pyv.simulator import Simulator
 
 @pytest.fixture
 def csr_block() -> CSRBlock:
-    csr = CSRBlock()
+    csr = CSRBlock(0)
     csr._init()
     return csr
 
@@ -29,6 +30,7 @@ class TestCSRBlock:
 def csr_unit() -> CSRUnit:
     csr_unit = CSRUnit()
     csr_unit._init()
+    Clock.reset()
     return csr_unit
 
 def csr_write(csr_unit: CSRUnit, csr_num: int, value: int, sim: Simulator):
@@ -38,14 +40,10 @@ def csr_write(csr_unit: CSRUnit, csr_num: int, value: int, sim: Simulator):
     sim.step()
 
 def csr_read(csr_unit: CSRUnit, csr_num: int, sim: Simulator):
-    csr_unit.read_en_i.write(True)
+    csr_unit.write_en_i.write(False)
     csr_unit.csr_num_i.write(csr_num)
     sim.step()
     return csr_unit.read_val_o.read()
-
-def write_and_read(csr_unit: CSRUnit, csr_num: int, value: int, sim: Simulator):
-    csr_write(csr_unit, csr_num, value, sim)
-    return csr_read(csr_unit, csr_num, sim)
 
 
 class TestCSRUnit:
@@ -68,20 +66,25 @@ class TestCSRUnit:
         sim.step()
         assert csr_unit.csr_bank.misa._csr_reg.cur._val == 0x1234
 
-    def test_read_and_write_at_same_time(self, sim: Simulator, csr_unit: CSRUnit):
-        csr_unit.read_en_i.write(True)
+    def test_read_and_write_in_same_cycle(self, sim: Simulator, csr_unit: CSRUnit):
+        csr_unit.csr_num_i.write(0x301)
+        csr_unit.write_en_i.write(False)
+        sim.step()
         csr_unit.write_en_i.write(True)
-        with pytest.raises(Exception):
-            sim.step()
+        csr_unit.write_val_i.write(0x42)
+        assert csr_unit.read_val_o.read() == 0x4000_0100
+        sim.step()
+        # Second step is required to run process method of CSROutMux
+        sim.step()
+        assert csr_unit.read_val_o.read() == 0x42
 
-    def test_non_existent_csr(self):
-        assert False, "Test needs to be implemented"
 
-    def test_access_to_debug_csr(self):
-        """Machine-mode standard read-write CSRs 0x7A0–0x7BF are reserved for use by the debug system.
-            Of these CSRs, 0x7A0–0x7AF are accessible to machine mode, whereas 0x7B0–0x7BF are only visible
-            to debug mode. Implementations should raise illegal instruction exceptions on machine-mode access
-            to the latter set of registers
-        """
-        assert False, "Test needs to be implemented"
+    def test_m_mode_csrs(self, sim: Simulator, csr_unit: CSRUnit):
+        # ---- misa ----
+        misa = 0x301
+        reset_val = csr_read(csr_unit, misa, sim)
+        assert reset_val == 0x4000_0100
 
+        csr_write(csr_unit, misa, 0xFFFF_FFFF, sim)
+        read_back = csr_read(csr_unit, misa, sim)
+        assert read_back == 0xFFFF_FFFF
