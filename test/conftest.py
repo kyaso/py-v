9d905fabcd1afe59432fc1a2e7ace0b520745fb4
Kirthihan Yasotharan <kirthihan.y@gmail.com>
2024-01-28 15:53:10 +0100

    style: PEP8

--- test/conftest.py
+++ test/conftest.py
@@ -1,10 +1,12 @@
 import pytest
 from pyv.simulator import Simulator
 
+
 @pytest.fixture(autouse=True)
 def enable_log():
     import logging
     logging.disable(logging.NOTSET)
+
 
 @pytest.fixture(autouse=True)
 def clear_sim():
